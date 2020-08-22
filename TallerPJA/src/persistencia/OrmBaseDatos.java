52342740a8ddf23ff56467e50a95c09e764ffb66
Estefania Castaño <estefaniacv26@gmail.com>
2020-08-22 00:50:44 -0500

    se crea la clase ListaEmpleados para que se cumpla los principios solid y facilitar las pruebas JUnit

--- TallerPJA/src/persistencia/OrmBaseDatos.java
+++ TallerPJA/src/persistencia/OrmBaseDatos.java
@@ -1,5 +1,38 @@
 package persistencia;
 
-public class OrmBaseDatos {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import entidades.Empleado;
+
+public class OrmBaseDatos implements RepositorioEmpleados {
+
+	private EntityManager gestorBd;
+	
+	
+	@Override
+	public List<Empleado> consultarEmpleados() {
+		
+		return null;
+	}
+
+
+	@Override
+	public boolean adicionarEmpleado(Empleado Empleado) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Empleado buscarEmpleado(int identificador) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean borrarEmpleado(Empleado empleado) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
