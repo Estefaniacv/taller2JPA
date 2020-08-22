f97781f11303d01e7b27f5909e79d70c867963f7
Estefania Castaño <estefaniacv26@gmail.com>
2020-08-22 01:06:09 -0500

    se crea en persistencia la clase ListaEmpleados para tener facilidad al realizar las pruebas y cumplir con los requerimientos solicitados en el ejercicio

--- TallerPJA/src/persistencia/ListaEmpleados.java
+++ TallerPJA/src/persistencia/ListaEmpleados.java
@@ -1,5 +1,33 @@
 package persistencia;
 
-public class ListaEmpleados {
+import java.util.List;
+
+import entidades.Empleado;
+
+public class ListaEmpleados implements RepositorioEmpleados {
+
+	@Override
+	public List<Empleado> consultarEmpleados() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean adicionarEmpleado(Empleado Empleado) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Empleado buscarEmpleado(int identificador) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean borrarEmpleado(Empleado empleado) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
